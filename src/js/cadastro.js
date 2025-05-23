fe21e3f0f37366cbc6dc20ac8755061485d240a1
Welington <welingtoncharnetjr@gmail.com>
2025-05-22 21:52:33 -0300

    Avançando no js

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,17 +1,28 @@
 var formProduto = document.getElementById("form-produto")
 
+var listaProdutos = []
+
 function cadastrarProduto(event) {
     event.preventDefault()
 
     var produto = document.getElementById("nome-produto").value
     var quantidade = document.getElementById("quantidade-produto").value
-    var imagem = document.getElementById("url-imagem").value
+    var imagem = document.getElementById("imagem-produto").value
 
     console.log(produto)
     console.log(quantidade)
     console.log(imagem)
 
+    var objetoProduto = {
+        nome: produto,
+        quantidade: quantidade,
+        imagem: imagem
+    }
+    
     console.log("Função cadastrarProduto chamada")
+
+    listaProdutos.push(objetoProduto)
+    console.log(listaProdutos)
 }
 
 formProduto.addEventListener("submit", cadastrarProduto)
